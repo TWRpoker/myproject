033f37fed923ef7476ad0a83b981fcc60bf303df
Chris <chris@realtime.tw>
2018-12-23 14:46:27 +0800

    Initial commit

---
+++ src/main/java/com/tom/Hello.java
@@ -0,0 +1,9 @@
+package com.tom;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("Hello 123");
+        Person person = new Person();
+        person.hello();
+    }
+}
